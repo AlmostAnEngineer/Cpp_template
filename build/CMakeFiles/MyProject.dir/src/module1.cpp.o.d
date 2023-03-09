2c6fb450cf72dfcac704780f0d35f62c23206a04
AlmostAnEngineer <94685050+AlmostAnEngineer@users.noreply.github.com>
2023-03-09 23:28:52 +0100

    Delete build directory

--- build/CMakeFiles/MyProject.dir/src/module1.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MyProject.dir/src/module1.cpp.o: \
-  /home/almostanengineer/Pulpit/C++\ template/Executable\ template/src/module1.cpp \
-  /home/almostanengineer/Pulpit/C++\ template/Executable\ template/src/module1.h
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/almostanengineer/Pulpit/C++ template/Executable template
-# Build directory: /home/almostanengineer/Pulpit/C++ template/Executable template/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
